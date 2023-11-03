a924f7ffa3031feb853674cd92be4c2f8b9c5474
akifkadioglu <akif.kadioglu.28@gmail.com>
2023-11-04 01:15:50 +0300

    UI değişiklikleri yaptım

--- src/components/home/navbar.vue
+++ src/components/home/navbar.vue
@@ -1,35 +1,42 @@
 <template>
-    <div class="mb-16 m-5">
-      <div class="mb-9">
-        <SwitchMode />
-      </div>
-      <div class="flex justify-center">
-        <span
-          :class="isDark ? 'linear_gradient' : ''"
-          class="font-montserrat-bold text-5xl"
-        >
-          <span> Akif </span>
-          <span :class="isDark ? '' : 'linear_gradient'"> Kadıoğlu </span>
-        </span>
-      </div>
+  <div class="mb-16 m-5">
+    <div class="flex items-center justify-between mb-9">
+      <a href="https://akifkadioglu.dev" target="_blank">
+        <img
+          class="rounded-full"
+          width="40"
+          src="https://raw.githubusercontent.com/akifkadioglu/akiftech/main/public/logo.png"
+          alt="me"
+        />
+      </a>
+      <SwitchMode />
     </div>
-  </template>
-  <script setup>
-  import { useDark } from "@vueuse/core";
-  import SwitchMode from "../switch_mode.vue";
-  
-  const isDark = useDark();
-  </script>
-  
-  <style scoped>
-  .linear_gradient {
-    background: -webkit-linear-gradient(45deg, #0072bc, #34d0db, #5bde7c);
-    background-clip: text;
-    -webkit-text-fill-color: transparent;
-  }
-  
-  hr {
-    border-top: 0.5px solid rgba(122, 122, 122, 0.452);
-  }
-  </style>
-  
+    <div class="flex justify-center">
+      <span
+        :class="isDark ? 'linear_gradient' : ''"
+        class="font-montserrat-bold text-5xl"
+      >
+        <span> Akif </span>
+        <span :class="isDark ? '' : 'linear_gradient'"> Kadıoğlu </span>
+      </span>
+    </div>
+  </div>
+</template>
+<script setup>
+import { useDark } from "@vueuse/core";
+import SwitchMode from "../switch_mode.vue";
+
+const isDark = useDark();
+</script>
+
+<style scoped>
+.linear_gradient {
+  background: -webkit-linear-gradient(45deg, #0072bc, #34d0db, #5bde7c);
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+hr {
+  border-top: 0.5px solid rgba(122, 122, 122, 0.452);
+}
+</style>
--- src/layouts/home.vue
+++ src/layouts/home.vue
@@ -6,12 +6,8 @@
     <div class="my-10">
       <slot />
     </div>
-    <div>
-      <Footer />
-    </div>
   </div>
 </template>
 <script setup>
-import Footer from "../components/home/footer.vue";
 import Navbar from "../components/home/navbar.vue";
 </script>
